a2773bd50b8bd082fcfa6e98e168c94e82eab265
Orla Woods <orla.woods@gmail.com>
2026-04-13 00:18:51 +0100

    Assignment 04 - replaced Andrew with my name

--- assignments/topic04/getrepoinformation.py
+++ assignments/topic04/getrepoinformation.py
@@ -1,9 +1,12 @@
 import requests
 import json
 
+#filename = "repos-public.json"
 filename = "wsaa-code.json"
 
+#url = 'https://api.github.com/users/andrewbeattycourseware/repos?per_page=100'
 url = 'https://api.github.com/repos/andrewbeattycourseware/wsaa-course-material/contents/code'
+#url = 'https://api.github.com/repos/andrewbeattycourseware/aprivateone'
 
 response = requests.get(url)
 print (response.status_code)
